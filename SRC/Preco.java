1f0f1d411899d9f7bcab74ff597267686772fdac
brunoharger <37717476+brunoharger@users.noreply.github.com>
2018-10-22 20:25:43 -0300

    Add files via upload

---
+++ SRC/Preco.java
@@ -0,0 +1,10 @@
+
+public abstract class Preco {
+	abstract public int getPreco();
+	   
+    abstract public double getValor(int diasAluguel);
+ 
+    public int getPontos(int diasAluguel) {
+          return 1;
+    }    
+}
